f316d8f7a0990d0841f14b2eeddfb35993050db8
Stéphan Gaud <stephgoeseast@gmail.com>
2016-11-23 12:16:32 +0000

    Add a function to echo back messages

--- app.js
+++ app.js
@@ -41,6 +41,25 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
