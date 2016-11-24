915cb890fac586cbd2f7593b202d06f6c9028395
Stéphan Gaud <stephgoeseast@gmail.com>
2016-11-24 15:06:59 +0000

    change for emoji

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/webhook/', function (req, res) {
       let sender = event.sender.id
       if (event.message && event.message.text) {
         let text = event.message.text
-        if (text === 'Cat') {
+        if (text === ':heart:') {
             sendCatMessage(sender)
             continue
         }
